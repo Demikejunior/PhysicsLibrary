aab8e01fbf1693749c7bb054f086cfdcfa5b7929
SpikeYr <emil.f.anderson@gmail.com>
2018-10-19 16:01:52 +0200

    commit 3 - Constants and fahrenheit to celsius

--- src/enums/Physics.java
+++ src/enums/Physics.java
@@ -2,10 +2,17 @@ package enums;
 
 public class Physics {
 	
-	
 	static double G = 9.82;
 	static double ATM = 101.3E3; // 101.3 kPa
-
+	static double R = 8.3144598;
+	static double p_0 = 101325;
+	static double c = 299792458;
+	static double g_swe = 9.82;
+	
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		return (fahrenheit - 32) / 1.8;
+	}
+	
 	
 	/**
 	 * Calculates the fluid pressure in a specified fluid at a specified depth. 
