db6f101123e50b4fa898a58836f2c342191b5862
SpikeYr <emil.f.anderson@gmail.com>
2018-11-07 21:18:39 +0100

    Revert "commit 19 - half done with JavaDocs"
    This reverts commit c1200423f9993c8072e2a0816297f9758e5c8c9a.

--- src/enums/Physics.java
+++ src/enums/Physics.java
@@ -10,10 +10,6 @@ public class Physics {
 	static double g_swe = 9.82;
 
 	/**
-	 * Calculates the temperature given - in fahrenheit - into celsius. <br> <br>
-	 * 
-	 * Parameter 1 is the temperature in fahrenheit. <br>
-	 * Returns the temperature in celsius.
 	 * 
 	 * @param fahrenheit
 	 * @return celsius
@@ -24,10 +20,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the temperature given - in celsius - into fahrenheit. <br> <br>
-	 * 
-	 * Parameter 1 is the temperature in celsius. <br>
-	 * Returns the temperature in fahrenheit.
 	 * 
 	 * @param celsius
 	 * @return fahrenheit
@@ -38,10 +30,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the temperature given - in kelvin - into celsius. <br> <br>
-	 * 
-	 * Parameter 1 is the temperature in kelvin. <br>
-	 * Returns the temperature in celsius.
 	 * 
 	 * @param kelvin
 	 * @return celsius
@@ -50,31 +38,15 @@ public class Physics {
 	public static double kelvinToCelsius(double kelvin) {
 		return kelvin - 273.15;
 	}
-	
-	/**
-	 * Calculates the temperature given - in celsius - into kelvin. <br> <br>
-	 * 
-	 * Parameter 1 is the temperature in celsius. <br>
-	 * Returns the temperature in kelvin.
-	 * 
-	 * @param celsius
-	 * @return kelvin
-	 */
 
-	public static double celsiusToKelvin(double celsius) {
-		return celsius + 273.15;
-	}
-	
 	/**
 	 * Calculates the fluid pressure in a specified fluid at a specified depth. <br> <br>
 	 * 
 	 * Parameter 1 is the type of fluid. <br>
-	 * Parameter 2 is how far down that the pressure is measured - in meters. <br>
-	 * Returns the pressure in newtons per square meter.
+	 * Parameter 2 is how far down - in meters - that the pressure is measured
 	 * 
 	 * @param fluid
 	 * @param depth
-	 * @return p
 	 */
 
 	public static double fluidPressure(FluidTable fluid, double depth) {
@@ -82,10 +54,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the fluid pressure in a specified fluid at a specified depth. <br> <br>
-	 * 
-	 * Parameter 1 is how far down that the pressure is measured - in meters. <br>
-	 * Returns the pressure in newtons per square meter.
 	 * 
 	 * @param depth
 	 * @return p
@@ -94,13 +62,7 @@ public class Physics {
 	public static double pressureUnderWater(double depth) {
 		return FluidTable.H2O.density * G * depth;
 	}
-	
 	/**
-	 * Calculates the amount of pressure on a specified area. <br> <br>
-	 * 
-	 * Parameter 1 is the force enacted on the area in newton. <br>
-	 * Parameter 2 is the area that force is acted upon in m^2. <br>
-	 * Returns the pressure in newtons per square meter.
 	 * 
 	 * @param force
 	 * @param area
@@ -112,10 +74,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the kinetic energy from the mass, and velocity of the object. <br> <br>
-	 * 
-	 * Parameter 1 is the mass of the object in kilograms. <br>
-	 * Parameter 2 is the velocity of the object in meters per second.
 	 * 
 	 * @param mass
 	 * @param velocity
@@ -127,11 +85,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the potential energy from the mass, and height of the object. <br> <br>
-	 * 
-	 * Parameter 1 is the mass of the object in kilograms. <br>
-	 * Parameter 2 is the height of the object in meters. <br>
-	 * Returns the potential energy in joules.
 	 * 
 	 * @param mass
 	 * @param height
@@ -143,10 +96,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the velocity of an object after it has fallen a specified length. <br> <br>
-	 * 
-	 * Parameter 1 is the length from the object to the specified point - regularly the ground - in meters. <br>
-	 * Returns the velocity at that point in meters per second.
 	 * 
 	 * @param height
 	 * @return velocity
@@ -157,11 +106,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the difference between two numbers. <br> <br>
-	 * 
-	 * Parameter 1 is the first number. <br>
-	 * Parameter 2 is the second number. <br>
-	 * Returns the difference between them.
 	 * 
 	 * @param first
 	 * @param last
@@ -173,11 +117,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the mass of a fluid from the type of fluid, and the volume of that fluid. <br> <br>
-	 * 
-	 * Parameter 1 is the fluid specified. <br>
-	 * Parameter 2 is the volume of the specified fluid in cubic meters. <br>
-	 * Returns the mass of the fluid in kilograms.
 	 * 
 	 * @param fluid
 	 * @param volume
@@ -189,11 +128,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the mass of a gas from the type of gas, and the volume of that gas. <br> <br>
-	 * 
-	 * Parameter 1 is the gas specified. <br>
-	 * Parameter 2 is the volume of the specified gas in cubic meters. <br>
-	 * Returns the mass of the gas in kilograms.
 	 * 
 	 * @param gas
 	 * @param volume
@@ -205,11 +139,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the mass of a solid from the the type of solid, and volume of that solid. <br> <br>
-	 * 
-	 * Parameter 1 is the solid specified. <br>
-	 * Parameter 2 is the volume of the specified solid in cubic meters. <br>
-	 * Returns the mass of the solid in kilograms.
 	 * 
 	 * @param solid
 	 * @param volume
@@ -221,14 +150,9 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the volume of a fluid from the type of fluid, and the mass of that fluid. <br> <br>
-	 * 
-	 * Parameter 1 is the fluid specified. <br>
-	 * Parameter 2 is the mass of the specified fluid in kilograms. <br>
-	 * Returns the volume of the fluid in cubic meters.
 	 * 
 	 * @param fluid
-	 * @param mass
+	 * @param kg
 	 * @return m^3
 	 */
 	
@@ -237,14 +161,9 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the volume of a gas from the type of gas, and the mass of that gas. <br> <br>
-	 * 
-	 * Parameter 1 is the gas specified. <br>
-	 * Parameter 2 is the mass of the specified gas in kilograms. <br>
-	 * Returns the volume of the gas in cubic meters.
 	 * 
 	 * @param gas
-	 * @param mass
+	 * @param kg
 	 * @return m^3
 	 */
 	
@@ -253,14 +172,9 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the volume of a solid from the type of solid, and the mass of that solid. <br> <br>
-	 * 
-	 * Parameter 1 is the solid specified. <br>
-	 * Parameter 2 is the mass of the specified solid in kilograms. <br>
-	 * Returns the volume of the solid in cubic meters.
 	 * 
 	 * @param solid
-	 * @param mass
+	 * @param kg
 	 * @return m^3
 	 */
 	
@@ -269,11 +183,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the velocity from the distance traveled, and the time taken. <br> <br>
-	 * 
-	 * Parameter 1 is the distance traveled in meters. <br>
-	 * Parameter 2 is the time taken. <br>
-	 * Returns the velocity in meters per second.
 	 * 
 	 * @param distance
 	 * @param time
@@ -285,11 +194,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the distance traveled from the velocity, and the time taken <br> <br>
-	 * 
-	 * Parameter 1 is the velocity in meters per second. <br>
-	 * Parameter 2 is the time taken. <br>
-	 * Returns the distance traveled in meters.
 	 * 
 	 * @param velocity
 	 * @param time
@@ -301,11 +205,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the  time taken from the distance traveled, and the velocity <br> <br>
-	 * 
-	 * Parameter 1 is the distance traveled in meters. <br>
-	 * Parameter 2 is the velocity in meters per second. <br>
-	 * Returns the time taken.
 	 * 
 	 * @param distance
 	 * @param velocity
@@ -317,27 +216,17 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the work of an action from the mass of the object acted upon, and the stretch that action takes the object. <br> <br>
-	 * 
-	 * Parameter 1 is the mass of the object in kilograms. <br>
-	 * Parameter 2 is the stretch the action takes the object in meters. <br>
-	 * Returns the work done in watt.
 	 * 
 	 * @param mass
-	 * @param s
+	 * @param acceleration
 	 * @return work
 	 */
 	
-	public static double work(double mass, double s) {
-		return mass * s;
+	public static double work(double mass, double acceleration) {
+		return mass * acceleration;
 	}
 
 	/**
-	 * Calculates the power of an action from the work done, and the time the action took. <br> <br>
-	 * 
-	 * Parameter 1 is the work done in watt. <br>
-	 * Parameter 2 is the time taken. <br>
-	 * Returns the power of the action.
 	 * 
 	 * @param work
 	 * @param time
@@ -349,11 +238,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the necessary energy to change the temperature in a solid. <br><br>
-	 * 
-	 * Parameter 1 is the type of solid. <br>
-	 * Parameter 2 is the mass of that solid in kilograms. <br>
-	 * Parameter 3 is the difference of temperature in celsius.
 	 * 
 	 * @param solid
 	 * @param mass
@@ -366,11 +250,6 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the necessary energy to change the temperature in a fluid. <br><br>
-	 * 
-	 * Parameter 1 is the type of fluid. <br>
-	 * Parameter 2 is the mass of that fluid in kilograms. <br>
-	 * Parameter 3 is the difference of temperature in celsius.
 	 * 
 	 * @param fluid
 	 * @param volume
@@ -383,11 +262,7 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the necessary energy to change the temperature in a gas. <br><br>
 	 * 
-	 * Parameter 1 is the type of gas. <br>
-	 * Parameter 2 is the mass of that gas in kilograms. <br>
-	 * Parameter 3 is the difference of temperature in celsius.
 	 * 
 	 * @param gas
 	 * @param volume
@@ -400,10 +275,8 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the peak of height that an object will get to in a vacuum from it's current velocity upwards <br> <br>
-	 * 
-	 * Parameter 1 is the current velocity upwards in meters per second. <br>
-	 * Returns the height that object gets to in meters.
+	 * Calculates the peak of height - in m -<br>
+	 * that an object will get to in a vacuum from it's current velocity upwards - in m/s
 	 * 
 	 * @param velocity
 	 * @return height
@@ -414,10 +287,7 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the mass from the measured weight. <br> <br>
-	 * 
-	 * Parameter 1 is the mass of the object in kilograms. <br>
-	 * Returns the weight of that object in newton.
+	 * Calculates the mass - kg - from the measured weight / force - newton
 	 * 
 	 * @param n
 	 * @return kg
@@ -428,7 +298,7 @@ public class Physics {
 	}
 
 	/**
-	 * Calculates the from the measured 
+	 * Calculates the weight - in newton - from the measured mass - in kg
 	 * 
 	 * @param n
 	 * @return kg
@@ -530,7 +400,7 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the necessary energy to change a fluid to a gas. <br> <br>
+	 * Calculates the necessary energy to change a fluid to a gas.
 	 * 
 	 * Parameter 1 is the type of fluid. Parameter 2 is the mass of that fluid.
 	 * 
@@ -544,9 +414,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates the necessary energy to change a solid to a fluid. <br> <br>
-	 * 
-	 * Parameter 1 is the type of solid. Parameter 2 is the mass of that solid.
 	 * 
 	 * @param solid
 	 * @param mass
@@ -558,7 +425,6 @@ public class Physics {
 	}
 	
 	/**
-	 * Calculates
 	 * 
 	 * @param g
 	 * @return
